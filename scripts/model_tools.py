b2cebc114df8a78146033a61dfd59338c4a11de0
carviagu <85288542+carviagu@users.noreply.github.com>
2021-12-17 20:18:37 +0100

    Added all curves to models and minor changes

--- scripts/model_tools.py
+++ scripts/model_tools.py
@@ -4,6 +4,7 @@
 
 from sklearn import metrics
 from matplotlib import pyplot as plt
+import scikitplot as skplt
 
 
 def accuracy(test=None, pred=None):
@@ -93,4 +94,44 @@ def roc_curve(test = None, pred = None, label = "Model"):
     plt.legend()
     # show the plot
     plt.show()
-    
+
+
+def model_curves_summary(test = None, pred = None, label = 'Model'):
+    '''
+    Prints a plot report with four different plots: ROC curve, PR curve,
+    Gain curve, Lift curve
+    :param test: Real values
+    :param pred: Predicted values
+    :param label: Model name
+    '''
+    fig, axes = plt.subplots(2, 2, figsize=(15, 10), sharey=False)
+    fig.suptitle('Curvas del modelo ' + label)
+
+    # ROC CURVE
+    fpr, tpr, thresholds = metrics.roc_curve(test, pred[:,1])
+    # plot the roc curve for the model
+    axes[0,0].plot([0,1], [0,1], linestyle='--', label='No Skill')
+    axes[0,0].plot(fpr, tpr, marker='.', label=label)
+    # axis labels
+    axes[0,0].set_title('Curva ROC')
+    axes[0,0].set(xlabel='False Positive Rate', ylabel='True Positive Rate')
+    axes[0,0].legend()
+    axes[0,0].grid()
+
+    # Precission-Recall CURVE
+    # calculate pr-curve
+    precision, recall, thresholds = metrics.precision_recall_curve(test, pred[:,1])
+    no_skill = len(test[test==1]) / len(test)
+    axes[0,1].plot([0,1], [no_skill,no_skill], linestyle='--', label='No Skill')
+    axes[0,1].plot(recall, precision, marker='.', label=label)
+    # axis labels
+    axes[0,1].set_title('Curva Precission-Recall')
+    axes[0,1].set(xlabel='Precission', ylabel='Recall')
+    axes[0,1].legend()
+    axes[0,1].grid()
+
+    # GAIN CURVE
+    skplt.metrics.plot_cumulative_gain(test, pred, ax = axes[1,0])
+
+    # LIFT CURVE
+    skplt.metrics.plot_lift_curve(test, pred, ax = axes[1,1])
